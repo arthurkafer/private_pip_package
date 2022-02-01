f26e8f3dde83d69e09a53532f96f0804e76f8b77
Arthur Kafer <arthurkafer@gmail.com>
2022-02-01 15:42:34 -0300

    teste dois pacotes, um com dependencia

---
+++ dependencia/pitico.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+class DetectorNP():
+	def __init__(self):
+		self.teste = np.zeros(5)
+	
+	def see(self):
+		return self.teste
--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 import setuptools
-print(setuptools.find_packages())
 
 setuptools.setup(
-    name="pacotinho",
+    name="packagemaior",
     version="0.0.1",
     author="arthurkafer",
     author_email="arthurkafer@gmail.com",
     description="pactinho teste",
-    long_description="logger basico em classe pra teste de install de pacote por pip",
+    long_description="logger basico em classe pra teste de install de pacote por pip, e outras cositas mas",
     url="https://github.com/arthurkafer/private_pip_package",
     packages=setuptools.find_packages(),
+	install_requires=[
+		'numpy'
+	],
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
