491cff258cdcb61eb584123cd96b50be31476ddc
Arthur Kafer <arthurkafer@gmail.com>
2022-02-01 14:30:20 -0300

    pacote adicionado

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="pacotinho",
+    version="0.0.1",
+    author="arthurkafer",
+    author_email="arthurkafer@gmail.com",
+    description="pactinho teste",
+    long_description="logger basico em classe pra teste de install de pacote por pip",
+    url="https://github.com/arthurkafer/private_pip_package",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
