ce623c152dd09922cb4a836f55db603ad42e4b1e
Arthur Kafer <arthurkafer@gmail.com>
2022-02-02 09:39:26 -0300

    teste __init__

--- dependencia/np.py
+++ dependencia/np.py
@@ -1,5 +1,4 @@
 import numpy as np
-import integer
 
 class DetectorNP():
 	def __init__(self):
--- setup.py
+++ setup.py
@@ -9,12 +9,6 @@ setuptools.setup(
     long_description="logger basico em classe pra teste de install de pacote por pip, e outras cositas mas",
     url="https://github.com/arthurkafer/private_pip_package",
     packages=setuptools.find_packages(),
-	extras_require={
-		'dependencia': [
-			'numpy',
-			'integer'
-		]
-	},
     classifiers=[
         "Programming Language :: Python :: 3",
     ],
